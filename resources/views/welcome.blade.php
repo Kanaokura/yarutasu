6a057eb7f7591fda7ee33f8cc75a4c8f60b17819
Kana <okura0405@gmail.com>
2020-04-22 23:15:19 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+@endsection
+
+@section('content')
+    <img src="{{ asset('images/yarutasu.png') }}" alt="" class="">
+@endsection
